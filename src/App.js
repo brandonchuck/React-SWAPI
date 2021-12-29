b64fa541b7bf40dd6bd422cdde8679aa6e137f58
Brandon Chuck <blc42310@gmail.com>
2021-12-28 21:07:05 -0800

    Created react project; Currently able to search a single character and have and object printed to the console with all of its attributes for the character; Added CharacterTable component to display data; Working on learning how to implement pagination;

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import CharacterTable from "./CharacterTable";
+
+const axios = require("axios");
 
 function App() {
+  const [character, setCharacter] = useState("");
+  const [characterName, setCharacterName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .get(`https://swapi.dev/api/people/?search=${characterName}`)
+      .then((res) => {
+        setCharacter(res);
+        console.log(res.data.results[0]);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>STAR WARS API</h1>
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          onChange={(e) => setCharacterName(e.target.value)}
+          value={characterName}
+        />
+        <input type="submit" />
+      </form>
+      <CharacterTable character={character} />
     </div>
   );
 }
 
 export default App;
+
+// Table requirements:
+// name
+// birth date
+// height
+// mass
+// homeworld
+// species
